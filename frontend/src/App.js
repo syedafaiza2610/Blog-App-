9b1efde25e53c44de73d5a6a3d71d58777ebe8bb
syeda Faiza <syedafaiza2610@gmail.com>
2024-07-08 00:32:00 +0500

    adding url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,42 @@
-
-import { useState, useEffect } from "react";
 import Form from "../src/components/blog";
+import { useState, useEffect } from "react";
+
 import { getAllBlogs, delete_Blog, updateBlogs, addBlogs } from "./controllers/handleApi";
-
-
 function App() {
   const [Blogs, setBlogs] = useState([]);
   const [text, setText] = useState("");
   const [title, setTitle] = useState("");
-   const [author, setAuthor] = useState("");
+  const [author, setAuthor] = useState("");
   const [isUpdating, setisUpdating] = useState(false);
-  const [blogId, setBlogsId] = useState("");
+  const [blogId, setBlogId] = useState("");
 
   useEffect(() => {
     getAllBlogs(setBlogs);
   }, []);
 
-  const updateMode = (_id, text , title , author) => {
+  const updateMode = (_id, text) => {
     setisUpdating(true);
     setText(text);
-    setTitle(title);
-    setAuthor(author);
-    setBlogsId(_id);
+    setBlogId(_id);
+  };
+
+  const handleAddOrUpdate = () => {
+    if (isUpdating) {
+      updateBlogs(blogId, text, setBlogs, setText, setisUpdating);
+    } else {
+      addBlogs({ title, author, text }, () => {
+        setTitle("");
+        setAuthor("");
+        setText("");
+        getAllBlogs(setBlogs);
+      });
+    }
   };
 
   return (
     <div className="App">
       <div className="container1">
-        <h1>Add Your Blog </h1>
+        <h1>Add Your Blog</h1>
         <div className="top">
           <input
             type="text"
@@ -35,36 +44,31 @@ function App() {
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
-          <br></br>
+          <br />
           <input
             type="text"
             placeholder="Author"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
           />
-          <br></br>
+          <br />
           <input
             type="text"
             placeholder="Add Your description"
             value={text}
             onChange={(e) => setText(e.target.value)}
           />
-          <div
-            className="add"
-            onClick={isUpdating
-              ? () => updateBlogs(blogId, text, setBlogs, setText, setisUpdating)
-              : () => addBlogs(text, setText, setBlogs)}
-          >
+          <div className="add" onClick={handleAddOrUpdate}>
             {isUpdating ? "Update" : "Add"}
           </div>
         </div>
         <div className="list">
           {Blogs.map((item) => (
-            <Form 
+            <Form
               key={item._id}
               title={item.title}
               author={item.author}
-              text={item.description}
+              text={item.text}
               updateMode={() => updateMode(item._id, item.description)}
               delete_Blog={() => delete_Blog(item._id, setBlogs)}
             />
@@ -76,5 +80,3 @@ function App() {
 }
 
 export default App;
-
-
--- frontend/src/components/blog.jsx
+++ frontend/src/components/blog.jsx
@@ -1,26 +1,4 @@
-// import React from 'react'
-// import { MdOutlineDeleteOutline } from "react-icons/md";
-// import { MdOutlineEdit } from "react-icons/md";
 
-// const Form = ({ text,title,author, delete_Blog, updateMode }) => {
-//   return (
-//     <div className='todo'>
-//       <div className='text'>{title}</div>
-//       <div className='text'>{author}</div>
-//       <div className='text'>{text}</div>
-
-//       <div className='icons'>
-//         <MdOutlineDeleteOutline className="icon" onClick={delete_Blog} />
-//         <MdOutlineEdit className="icon" onClick={updateMode} />
-
-//       </div>
-
-//     </div>
-//   )
-// }
-
-// export default Form;
-// ../src/components/blog.js
 import React from 'react';
 
 const Form = ({ title, author, text, updateMode, delete_Blog }) => {
@@ -31,9 +9,10 @@ const Form = ({ title, author, text, updateMode, delete_Blog }) => {
         <p>Author: {author}</p>
         <p>Description: {text}</p>
       </div>
-      <div className="icons">
-        <span className="icon" onClick={updateMode}>Update</span>
-        <span className="icon" onClick={delete_Blog}>Delete</span>
+      <div className='icons'>
+       <MdOutlineDeleteOutline className="icon" onClick={delete_Blog} />
+       <MdOutlineEdit className="icon" onClick={updateMode} />
+
       </div>
     </div>
   );
